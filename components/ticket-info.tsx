d1a6e8cd8487b5e911f6b1906c95abaf394f923f
GeneGulanesJr <ggulanes_180000000405@uic.edu.ph>
2021-07-12 08:34:55 +0800

    some stuff

--- components/ticket-info.tsx
+++ components/ticket-info.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 NT.GG Inc.
+ * Copyright 2020 Vercel Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,32 +18,32 @@ import styles from './ticket-info.module.css';
 import styleUtils from './utils.module.css';
 import Logo from './logo';
 import { DATE, SITE_URL } from '@lib/constants';
-import vercelLogo from '@components/icons/icon-platform';
+import VercelLogo from '@components/icons/icon-platform';
 
 const siteUrl = new URL(SITE_URL);
 const siteUrlForTicket = `${siteUrl.host}${siteUrl.pathname}`.replace(/\/$/, '');
 
 export default function TicketInfo({ logoTextSecondaryColor = 'var(--accents-5)' }) {
-  const createdBy = (
-    <div className={styles['created-by']}>
-      <div className={styles['created-by-text']}>Created by </div>
-      <div className={styles['created-by-logo']}>
-        <vercelLogo height="100%" color="var(--accents-4)" />
-      </div>
-    </div>
-  );
-  return (
-    <div className={styles.info}>
-      <div className={styles.logo}>
-        <Logo textSecondaryColor={logoTextSecondaryColor} />
-      </div>
-      <div className={styles.date}>
-        <div>{DATE}</div>
-        <div>ONLINE</div>
-      </div>
-      <div className={styleUtils['hide-on-mobile']}>{createdBy}</div>
-      <div className={styles.url}>{siteUrlForTicket}</div>
-      <div className={styleUtils['show-on-mobile']}>{createdBy}</div>
-    </div>
-  );
+    const createdBy = (
+        <div className={styles['created-by']}>
+            <div className={styles['created-by-text']}>Created by </div>
+            <div className={styles['created-by-logo']}>
+                <VercelLogo height="100%" color="var(--accents-4)" />
+            </div>
+        </div>
+    );
+    return (
+        <div className={styles.info}>
+            <div className={styles.logo}>
+                <Logo textSecondaryColor={logoTextSecondaryColor} />
+            </div>
+            <div className={styles.date}>
+                <div>{DATE}</div>
+                <div>ONLINE</div>
+            </div>
+            <div className={styleUtils['hide-on-mobile']}>{createdBy}</div>
+            <div className={styles.url}>{siteUrlForTicket}</div>
+            <div className={styleUtils['show-on-mobile']}>{createdBy}</div>
+        </div>
+    );
 }
